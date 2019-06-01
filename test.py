259e99fb0baa3a3d9c512a650e993067ca6d8422
Taro <morita0079@gmail.com>
2019-06-02 02:38:19 +0900

    add coment

--- test.py
+++ test.py
@@ -13,14 +13,14 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-instance = dht11.DHT11(pin=17)
+instance = dht11.DHT11(pin=17) # select pin
 
 try:
     while True:
         result = instance.read()
         if result.is_valid():
-            display.lcd_display_string("Temperature:" + str(result.temperature),1)
-            display.lcd_display_string("Humidity:" + str(result.humidity), 2)
+            display.lcd_display_string("Temperature:" + str(result.temperature),1) # write temperature in first line of LCD
+            display.lcd_display_string("Humidity:" + str(result.humidity), 2) # write humidity in second line of LCD
 
 except KeyboardInterrupt:
     display.lcd_clear()
